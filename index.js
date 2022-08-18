c4ccc9ba0977546cf7ea8266c152fa40636f5de6
Vishal Shetty <vishal.rajesh.shetty@gmail.com>
2022-08-18 12:14:53 +0530

    fix: resolved cors error

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
+const cors = require("cors");
 const port = 4000;
 
 const app = express();
-
+app.use(cors());
 app.use(
   "/graphql",
   graphqlHTTP({
